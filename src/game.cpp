e8b02ff5611a2a4b9f13707aa1fd98523bccae1c
Christopher Miller <admin@mctherealm.net>
2018-07-25 11:08:26 -0400

    Adjusted Window Size and title

--- src/game.cpp
+++ src/game.cpp
@@ -8,8 +8,8 @@
 #include <string>
 
 Game::Game(){
-    const char *title = "test";
-    Engine = new Madd(800, 600, title);
+    const char *title = "Miner1.5";
+    Engine = new Madd(1240, 698, title);
     GameObject* gameCamera = new GameCamera(Engine);
     Engine->AddObject(gameCamera);
 
@@ -19,7 +19,8 @@ Game::Game(){
     Manager->Register(new Block("Stone", "stone.jpg"));
     Manager->Register(new Block("Lobby Stone", "lobby.jpg"));
 
-    Manager->Place(new PlacedBlock(Manager->GetBlock("Lobby Stone").id, glm::vec3( 1.0f,  0.0f,  0.0f)));
+    Manager->Place(new PlacedBlock(Manager->GetBlock("Air").id, glm::vec3( 1.0f,  0.0f,  0.0f)));
+    Manager->Place(new PlacedBlock(Manager->GetBlock("Lobby Stone").id, glm::vec3( 2.0f,  0.0f,  0.0f)));
 
     Engine->AddObject(Manager);
 }
